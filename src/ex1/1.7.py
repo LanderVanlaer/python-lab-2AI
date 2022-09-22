088918c69374b0e168ecc475950959cd4493d812
Lander Van laer <lander.vanlaer1@gmail.com>
2022-09-22 15:30:13 +0200

    feat: ex1-1.7

---
+++ src/ex1/1.7.py
@@ -0,0 +1,6 @@
+# Write a program that reads your name and first name separately and prints them on one line. Test
+# your program using a name containing a whitespace (e.g. Van Bauwel).
+
+firstname = input("firstname: ")
+lastname = input("lastname: ")
+print(f"{firstname} {lastname}")
