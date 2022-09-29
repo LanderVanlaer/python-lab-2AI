21a368b6bb6dbcb96675d4de3bd8540e4683b04b
Lander Van laer <lander.vanlaer1@gmail.com>
2022-09-29 10:09:54 +0200

    feat: ex2-2.0

---
+++ src/ex2/2.0.py
@@ -0,0 +1,9 @@
+# Write a program that reads an integer from the console, stores it in a variable and prints whether it
+# is positive or negative.
+
+integer = int(input("Enter an integer: "))
+
+if integer >= 0:
+    print("Positive")
+else:
+    print("Negative")
