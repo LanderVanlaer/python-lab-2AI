670b72cd680dec0cca538d604819fc2ee279d87a
Lander Van laer <lander.vanlaer1@gmail.com>
2022-10-06 11:42:23 +0200

    feat: ex3-3.2

---
+++ src/ex3/2.py
@@ -0,0 +1,8 @@
+# Write a function “printStrSum” that accepts 2 integers in string form, computes their sum and prints
+# it in console.
+
+def printStrSum(a, b):
+    print(f"{a} + {b} = {int(a) + int(b)}")
+
+
+printStrSum(2, 3)
