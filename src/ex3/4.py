1219a537fdb3c7b4114fd525e20b8d5f01f57d7f
Lander Van laer <lander.vanlaer1@gmail.com>
2022-10-06 11:47:50 +0200

    feat: ex3-3.4

---
+++ src/ex3/4.py
@@ -0,0 +1,13 @@
+# Write a function “printOddEven” that accepts an integer as argument and prints if the number is
+# even or odd.
+
+def printOddEven(n):
+    if n % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+printOddEven(-1)
+printOddEven(0)
+printOddEven(2)
