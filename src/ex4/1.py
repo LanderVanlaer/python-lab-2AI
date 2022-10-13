b2b891a8c6ade58f0d0af2e313c4ba0584b8a6ad
Lander Van laer <lander.vanlaer1@gmail.com>
2022-10-13 11:57:42 +0200

    feat: ex4-4.1

---
+++ src/ex4/1.py
@@ -0,0 +1,5 @@
+# Write a program that asks a username. Revert the username using slicing and print it. Do not use
+# a loop for this program!
+
+username: str = input("Enter your username: ")
+print(username[::-1])
